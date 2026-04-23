db9bc347f0d0b71de24ac8533b80163ce9ac3749
콩승현 <kongseunghyun@kongbook.local>
2026-04-23 12:59:04 +0900

    feat: 실내 평면도 추가

--- SSP/app/app.py
+++ SSP/app/app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # 메인 대시보드 페이지 렌더링
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # 디버그 모드로 실행 (포트 5000)
     app.run(debug=True, host='0.0.0.0', port=5000)
